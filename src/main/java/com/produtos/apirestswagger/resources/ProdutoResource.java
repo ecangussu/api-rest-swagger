f3f81bd4c723e9e273c7eb655fd818c64da55809
Estevao Souza <estevaosouza18@gmail.com>
2021-08-07 17:35:11 -0300

    ProdutoResource - get e post

---
+++ src/main/java/com/produtos/apirestswagger/resources/ProdutoResource.java
@@ -0,0 +1,40 @@
+package com.produtos.apirestswagger.resources;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.apirestswagger.models.Produto;
+import com.produtos.apirestswagger.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoResource {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produto/{id}")
+	public Produto listaProduto(@PathVariable long id) {
+		Optional<Produto> obj = produtoRepository.findById(id);
+		return obj.get();
+	}
+	
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+}
