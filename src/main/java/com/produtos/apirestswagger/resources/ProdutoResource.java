6d2df659d6cccdb32c6fe48f397274f3c8fa4d17
Estevao Souza <estevaosouza18@gmail.com>
2021-08-07 18:28:58 -0300

    Adicionado o swagger

--- src/main/java/com/produtos/apirestswagger/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirestswagger/resources/ProdutoResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,35 +17,47 @@ import org.springframework.web.bind.annotation.RestController;
 import com.produtos.apirestswagger.models.Produto;
 import com.produtos.apirestswagger.repositories.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Produtos")
+@CrossOrigin(origins="*")
+//Libera o acesso da API por qualquer domínio
+//Acesso: http://localhost:8080/swagger-ui.html
 public class ProdutoResource {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produtos")
+	@ApiOperation(value="Retorna uma lista de produtos")
 	public List<Produto> listaProdutos() {
 		return produtoRepository.findAll();
 	}
 	
 	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna um produto")
 	public Produto listaProduto(@PathVariable long id) {
 		Optional<Produto> obj = produtoRepository.findById(id);
 		return obj.get();
 	}
 	
 	@PostMapping("/produto")
+	@ApiOperation(value="Salva um novo produto")
 	public Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
 	@PutMapping("/produto")
+	@ApiOperation(value="Atualiza um produto")
 	public Produto atualizaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
 	@DeleteMapping("/produto/{id}")
+	@ApiOperation(value="Deleta um produto")
 	public void deletaProduto(@PathVariable long id) {
 		produtoRepository.deleteById(id);
 	}
