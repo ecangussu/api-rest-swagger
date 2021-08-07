b7dfe19c254e57f34f1790ca9fff0b583d8f08aa
Estevao Souza <estevaosouza18@gmail.com>
2021-08-07 16:28:51 -0300

    criacao do projeto

---
+++ src/main/java/com/produtos/apirestswagger/ApiRestSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.produtos.apirestswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSwaggerApplication.class, args);
+	}
+
+}
